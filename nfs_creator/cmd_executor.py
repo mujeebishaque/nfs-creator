e408b3d939c041db829530f184200e9088ebb728
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-06 16:16:04 +0100

    cmd executor

--- nfs_creator/cmd_executor.py
+++ nfs_creator/cmd_executor.py
@@ -9,7 +9,8 @@ Return Type: Tuple
 import subprocess
 
 class BashExecutor:
-
+    ''' A static class to execute bash commands and return the output and error as a tuple.'''
+    
     @staticmethod
     def execute_cmd(_cmd):
         _p = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
