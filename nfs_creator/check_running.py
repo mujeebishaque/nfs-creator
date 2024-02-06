a9ab1c0c2a50f4c67baddc948eba2d295d0e3e62
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-06 17:24:11 +0100

    check_install and check_running updated

---
+++ nfs_creator/check_running.py
@@ -0,0 +1,19 @@
+'''
+nfs_creator.check_running
+==========================
+
+This module contains functions to check if the nfs service is running.
+'''
+import 
+
+class NFSServiceChecker:
+    ''' A class to check if the nfs service is running. '''
+    
+    @staticmethod
+    def is_running():
+        out, _ = subprocess.Popen(['systemctl', 'is-active', 'nfs'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+        
+        if 'inactive' in str(out):
+            print("❌ : nfs service is not running!")
+            return False
+        return True
