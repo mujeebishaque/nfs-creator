a7a206761c27c995167382137905210a465db57a
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-06 16:12:01 +0100

    headers added

--- nfs_creator/check_install.py
+++ nfs_creator/check_install.py
@@ -1,3 +1,6 @@
 '''
-nfs_creator.utils
+nfs_creator.check_install
+==========================
+
+This module contains functions to check if the nfs-utils is installed.
 '''
---
+++ nfs_creator/nfs_creator.py
@@ -0,0 +1,15 @@
+'''
+nfs_creator.nfs_creator
+=========================
+
+This module contains the main functions for the nfs-creator package.
+'''
+
+class NFSCreator:
+    
+    def __init__(self):
+        pass
+    
+    
+if __name__ == "__main__":
+    pass
