63fb58d48e1a8fb3b6db04ff51e91036cf90d644
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-08 18:37:31 +0100

    nfs_installer complete

--- nfs_creator/nfs_creator.py
+++ nfs_creator/nfs_creator.py
@@ -7,8 +7,9 @@ This module contains the main functions for the nfs-creator package.
 
 class NFSCreator:
     
-    def __init__(self):
+    def __init__(self, export_dir: str = '/var/data/'):
         pass
+    
     
     
 if __name__ == "__main__":
--- nfs_creator/nfs_installer.py
+++ nfs_creator/nfs_installer.py
@@ -4,26 +4,40 @@ nfs_creator.nfs_installer
 
 This module contains functions to install the nfs-utils package.
 '''
-import subprocess
+
+import sys, platform
+from nfs_creator.cmd_executor import BashExecutor
 
 class NFSInstaller:
     
+    rhel_based = ('CentOS Linux', 'Red Hat Enterprise Linux', 'Fedora')
+    rhel_installation_cmd = 'sudo yum install nfs-utils -y'
+    
+    debian_based = ('Ubuntu', 'Debian', 'Linux Mint')
+    debain_installation_cmd = 'sudo apt-get install nfs-common -y'
+    
     @staticmethod
-    def install_nfs(operating_system: str):
-        ''' Install the nfs-utils package on the system. '''
+    def install_nfs_package():
+        ''' A function to install the nfs-utils package. '''
         
-        if operating_system in ('CentOS Linux', 'Red Hat Enterprise Linux', 'Fedora'):
-            print('Installing nfs-utils package...')
-            out, _ = subprocess.Popen(['sudo', 'yum', 'install', 'nfs-utils', '-y'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            print(out.decode('utf-8'))
-            print('nfs-utils package installed successfully!')
+        if platform.system() == 'Linux':
             
-        elif operating_system in ('Ubuntu', 'Debian', 'Linux Mint'):
-            print('Installing nfs-common package...')
-            out, _ = subprocess.Popen(['sudo', 'apt-get', 'install', 'nfs-common', '-y'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            print(out.decode('utf-8'))
-            print('nfs-common package installed successfully!')
-            
+            if platform.linux_distribution()[0] in NFSInstaller.rhel_based:
+                
+                output, error = BashExecutor.execute_cmd(NFSInstaller.rhel_installation_cmd)
+                if 'Complete!' in output.decode('utf-8'):
+                    print('✅ : nfs-utils has been installed successfully!')
+                if error.decode('utf-8'):
+                    sys.exit(f'❌ : An error occurred while installing nfs-utils!. {error.decode("utf-8")}')
+                    
+                    
+            elif platform.linux_distribution()[0] in NFSInstaller.debian_based:
+                
+                output, error = BashExecutor.execute_cmd(NFSInstaller.debain_installation_cmd)
+                if error.decode('utf-8'):
+                    sys.exit(f'❌ : An error occurred while installing nfs-common!. {error.decode("utf-8")}')
+            else:
+                sys.exit('This script is only compatible with RPM-based and Debian-based Linux distributions.')
+        
         else:
-            print('This script is only compatible with RPM-based and Debian-based Linux distributions.')
-            return None
+            sys.exit('Please run this script on a Linux machine.')
