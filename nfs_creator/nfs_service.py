d3b7726f6df716c571cab069ea6283739e495c89
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-10 15:04:51 +0100

    nfs service enabler updated

--- nfs_creator/nfs_service.py
+++ nfs_creator/nfs_service.py
@@ -10,16 +10,16 @@ from cmd_executor import BashExecutor
 from package_installer import CURRENT_OS, RHEL_BASED_OS, DEBIAN_BASED_OS
 from nfs_creator.nfs_installation_checker import NFSInstallChecker
 
+
 class NFSService:
-    ''' A class to check if the nfs service is running and run it if it's not.
-        - I don't check if service is running, just enable and run it anyway.
-    '''
+    ''' Enable and run the service on the system.'''
     
     debian_start_cmd = "sudo systemctl start nfs-kernel-server"
     debian_enable_cmd = "sudo systemctl enable nfs-kernel-server"
     
     rhel_start_cmd = "sudo systemctl start nfs-server"
     rhel_enable_cmd = "sudo systemctl enable nfs-server"
+    
     
     @staticmethod
     def enable_start_service():
@@ -32,15 +32,15 @@ class NFSService:
                 if error.decode('utf-8'):
                     sys.exit("❌ Error starting nfs-kernel-server: ", error)
                 output, error = BashExecutor.execute_cmd(NFSService.debian_enable_cmd)
-                if error.decode:
+                if error.decode('utf-8'):
                     sys.exit("❌  Error enabling nfs-kernel-server: ", error)
             
             elif CURRENT_OS in RHEL_BASED_OS:
                 output, error = BashExecutor.execute_cmd(NFSService.rhel_start_cmd)
-                if error.decode:
+                if error.decode('utf-8'):
                     sys.exit("❌ Error starting nfs-server: ", error)
                 output, error = BashExecutor.execute_cmd(NFSService.rhel_enable_cmd)
-                if error.decode:
+                if error.decode('utf-8'):
                     sys.exit("❌ Error enabling nfs-server: ", error)
             
             else:
