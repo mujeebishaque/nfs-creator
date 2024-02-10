1a7ca2a13959897432cd107259879a15126fc134
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-10 21:36:36 +0100

    fixed installation_checker

--- nfs_creator/nfs_installation_checker.py
+++ nfs_creator/nfs_installation_checker.py
@@ -5,38 +5,35 @@ nfs_creator.check_install
 This module contains functions to check if the nfs-utils is installed.
 '''
 
-import sys, platform, subprocess
+import sys, subprocess
+from nfs_creator.package_installer import (
+    CURRENT_OS, RHEL_BASED_OS, DEBIAN_BASED_OS
+    )
 
 class NFSInstallChecker:
     ''' A class to check if the nfs-utils package is installed. '''
     
     @staticmethod
-    def is_linux():
-        return platform.system() == 'Linux'
-    
-    @staticmethod
     def is_installed():
-        if NFSInstallChecker.is_linux():
-            if platform.linux_distribution()[0] in ('CentOS Linux', 'Red Hat Enterprise Linux', 'Fedora'):
         
-                out, _ = subprocess.Popen(['rpm -q nfs-utils'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
-        
-                if 'not' in str(out):
-                    print("❌ : nfs-utils is not installed on this system!")
-                    return False
-                return True
-                
-            elif platform.linux_distribution()[0] in ('Ubuntu', 'Debian', 'Linux Mint'):
-        
-                out, _ = subprocess.Popen(['dpkg -s nfs-common | grep Status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
-
-                if 'not installed' in str(out):
-                    print("❌ : nfs-common not installed on this system!")
-                    return False
-                return True
+        if CURRENT_OS in RHEL_BASED_OS:
+    
+            out, _ = subprocess.Popen(['rpm -q nfs-utils'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+    
+            if 'not' in str(out):
+                print("❌ : nfs-utils is not installed on this system!")
+                return False
+            return True
             
-            else:
-                sys.exit('This script is only compatible with RPM-based and Debian-based Linux distributions.')
-                
-        sys.exit('Please run this script on a Linux machine.')
+        elif CURRENT_OS in DEBIAN_BASED_OS:
+    
+            out, _ = subprocess.Popen(['dpkg -s nfs-common | grep Status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
+
+            if 'not installed' in str(out):
+                print("❌ : nfs-common not installed on this system!")
+                return False
+            return True
         
+        else:
+            sys.exit('This script is only compatible with RPM-based and Debian-based Linux distributions.')
+                    
