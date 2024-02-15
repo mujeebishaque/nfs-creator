315d38daca67ce1a5e1aa609215bd3f109c2c170
A. Mujeeb <abdul.mujeeb@progressivesystems.it>
2024-02-15 03:04:15 +0100

    firewall cmd updated for rhel

--- nfs_creator/firewall_settings.py
+++ nfs_creator/firewall_settings.py
@@ -7,15 +7,19 @@ This module contains functions to configure the firewall settings for the nfs se
 
 import sys
 from nfs_creator.cmd_executor import BashExecutor
-
+from nfs_creator.package_installer import is_debian_based, is_rhel_based
 
 def configure_firewall_nfs():
     ''' A function to configure the firewall settings for the nfs service. '''
     
     print('⚠ : Attemping to install firewalld ...')
     
-    _, _ = BashExecutor.execute_cmd('sudo apt install firewalld -y')
+    if is_debian_based():
+        _, _ = BashExecutor.execute_cmd('sudo apt install firewalld -y')
     
+    if is_rhel_based():
+        _, _ = BashExecutor.execute_cmd('sudo yum install firewalld -y')
+
     
     print('⚠ : Configuring firewall settings...')
     
